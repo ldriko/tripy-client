72bfcbd14fad1f1bb752fd2ed2cc9c2940f54111
Heaven Aldrico <4d3.h34v3n@gmail.com>
2022-12-20 00:07:34 +0700

    Add icon component

---
+++ components/app-svg.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  width: {
+    type: Number,
+    default: null
+  },
+  height: {
+    type: Number,
+    default: null
+  }
+})
+</script>
+
+<template>
+  <img :alt="name" :height="height" :src="`/icons/${name}.svg`" :width="width">
+</template>
