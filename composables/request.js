8b33ddde9ceac4496746b498808a7e08f3c63b84
Heaven Aldrico <4d3.h34v3n@gmail.com>
2022-12-19 21:35:46 +0700

    Add tailwind and composed fetch

---
+++ composables/request.js
@@ -0,0 +1,36 @@
+export const useFetchCookies = async () => {
+  const config = useRuntimeConfig()
+
+  await $fetch.raw('/../sanctum/csrf-cookie', {
+    baseURL: config.public.baseURL,
+    credentials: 'include'
+  })
+}
+
+export const useRequest = async (url, options) => {
+  const config = useRuntimeConfig()
+  const csrf_cookie = 'XSRF-TOKEN'
+  let token = useCookie(csrf_cookie)?.value
+
+  if (!token) {
+    await useFetchCookies()
+
+    token = useCookie(csrf_cookie).value
+  }
+
+  const headers = {
+    Accept: 'application/json',
+    'Cache-Control': 'no-cache',
+    'X-XSRF-TOKEN': token,
+    ...options?.headers
+  }
+  const opts = options ? (({ headers, ...opts }) => opts)(options) : null
+  const baseURL = !options?.baseURL ? config.public.baseURL : options.baseURL
+
+  return useFetch(url, {
+    baseURL: baseURL,
+    credentials: 'include',
+    headers,
+    ...opts
+  })
+}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({})
+export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      baseURL: 'http://localhost/v1'
+    }
+  },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ]
+})
